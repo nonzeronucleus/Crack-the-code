6ac1e566684d71af41fe1ed7a8039c4a19a881d0
Ian <nonzeronucleus@ianplumb.org>
2022-03-27 21:06:29 +0100

    Initial Commit

---
+++ Crack The Code/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crack The Code
+//
+//  Created by Ian Plumb on 27/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crack The Code/Crack_The_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crack_The_CodeApp.swift
+//  Crack The Code
+//
+//  Created by Ian Plumb on 27/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Crack_The_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
